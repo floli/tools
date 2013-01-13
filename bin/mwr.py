8d7f5b9dd990f6a56d0d198bf3d256e9370ac32c
Florian Lindner <florian.lindner@xgm.de>
2013-01-13 19:32:13 +0100

    Added mwr.py (make world readable). A tool to make a path hierarchy world readable.

---
+++ bin/mwr.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python2
+
+""" Scripts that sets the permission of all directories up to path and all files and directories down from path to to o+xr. """
+
+import os, stat, sys
+
+def norm_path(*parts):
+    """ Returns the normalized, absolute, expanded and joint path, assembled of all parts. """
+    return os.path.abspath(os.path.expanduser(os.path.join(*parts)))
+
+def concat_iter(iterable, join = ""):
+    """ An iterator which returns more and more of an iterable. range(4) would return: "0", "01", "012" and "0123". """
+    result = ""
+    yield join
+    for i in iterable[1:]:
+        result = join.join([result, i])
+        yield result
+
+
+
+path = norm_path(sys.argv[1])
+print path
+
+for i in concat_iter(path.split(os.path.sep), join=os.path.sep):
+    st = os.stat(i).st_mode
+    if stat.S_ISDIR(st):
+        if (st & stat.S_IROTH) and (st & stat.S_IXOTH):
+            print "Not setting permissions on %s" % i
+        else:
+            print "Setting permissions on %s" % i
+            os.chmod(i, st | stat.S_IROTH | stat.S_IXOTH)
+
+   
+for root, dirs, files in os.walk(path):
+    for d in dirs:
+        d = os.path.join(root, d)
+        st = os.stat(d).st_mode
+        if (st & stat.S_IROTH) and (st & stat.S_IXOTH):
+            print "Not setting permissions on %s" % d
+        else:
+            print "Setting permissions on %s" % d
+            os.chmod(f, st | stat.S_IROTH | stat._SIXOTH)
+
+            
+    for f in files:
+        f = os.path.join(root, f)
+        st = os.stat(f).st_mode
+        if (st & stat.S_IROTH):
+            print "Not setting permissions on %s" % f
+        else:
+            print "Setting permissions on %s" % f
+            os.chmod(f, st | stat.S_IROTH)
+           
