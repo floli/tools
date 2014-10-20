c79a981ce0199c6d2b8435e027c38d3b798705f1
Florian Lindner <florian.lindner@ipvs.uni-stuttgart.de>
2014-10-20 15:13:57 +0200

    Script to time program executions and print stats.

---
+++ bin/timestats.py
@@ -0,0 +1,70 @@
+#!env python2
+
+import argparse, logging, subprocess, sys, time
+import numpy as np
+
+log = logging.getLogger(sys.argv[0])
+
+def setup_argparse():
+    parser = argparse.ArgumentParser(description='Times processes.')
+    parser.add_argument("--rep", "-m", default=1, type=int, help='Number of repetitions')
+    parser.add_argument("--time", "-t", default=0, type=float, help='Maximum total runtime of timing tests')
+    parser.add_argument("--verbose", "-v", action="store_const", const=0, default=15, help='Generate more output')
+    parser.add_argument("command", help="Command to execute")
+    return parser
+
+def setup_logging(level):
+    # CRITICAL	50
+    # ERROR	40
+    # WARNING	30
+    # INFO	20
+    # DEBUG	10
+    # NOTSET	0
+    logging.basicConfig(level=level)
+
+
+def execute(cmd):
+    log.debug("Executing command: %s", cmd)
+    t0 = time.time()
+    ret_code = subprocess.call(cmd, shell = True)
+    t1 = time.time()
+    log.info("Executing command took {:5.4} seconds.".format(t1 - t0))
+    if ret_code != 0:
+        log.warning("%s returned with %s", cmd, ret_code)
+    return t1 - t0
+
+def measure(cmd, max_time, max_rep):
+    exec_counter = 0
+    max_time = max_time if max_time else sys.maxsize
+    times = []
+
+    t0 = time.time()
+    while(True):
+        exec_counter += 1
+        log.debug("Repetition {} of {}, time elapsed {:.4} of max {}".format(exec_counter, max_rep,  time.time() - t0, max_time))
+        times.append(execute(cmd))
+        if exec_counter >= max_rep:
+            log.info("Maximum number of %s repetitions reached.", max_rep)
+            break
+        if time.time() - t0 > max_time:
+            log.info("Maximum execution time of %s seconds reached.", max_time)
+            break
+
+    total_time =  time.time() - t0
+    return times        
+        
+
+    
+def main():
+    parser = setup_argparse()
+    args = parser.parse_args()
+    setup_logging(args.verbose)
+    times = measure(args.command, args.time, args.rep)
+    print("Execution of %s finished." % args.command)
+    print("Total time: {:.4} seconds.".format(np.sum(times)))
+    print("Max: {:.4}, Min: {:.4}, Mean: {:.4}, StdDev: {:.6f}".format(np.max(times), np.min(times), np.mean(times), np.std(times)))
+    log.debug(times)
+
+if __name__ == "__main__":
+    main()
+
